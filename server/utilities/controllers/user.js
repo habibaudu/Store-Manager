2e3272c62a641be08e31ae20d979a022bc25df97
habib <habib.bincom@gmail.com>
2018-10-22 07:35:41 +0100

    update user.js use a foreach loop instead of a for of loop

--- server/utilities/controllers/user.js
+++ server/utilities/controllers/user.js
@@ -1,30 +1,39 @@
-import express from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import users from '../dummy_data/user.json';
-import config from '../configkey/key';
 
-const app = express();
-app.set('superSecret', config.secret);
+
+dotenv.config({ path: '/Users/HabibAudu/Documents/boot/Store-Manager/server/.env' });
 
 export default {
   login(req, res) {
     let token;
     let message;
-    for (const user of users) {
+    users.forEach((user) => {
       if (user.username !== req.body.username) {
         message = 'Wrong Name';
+        return res.status(403).json({
+          message
+        });
+        
       } else if (user.password !== req.body.password) {
         message = 'Wrong Password';
-        break;
+        return res.status(403).json({
+          message
+        });
       } else {
         // create the token.
-        token = jwt.sign(user, app.get('superSecret'), {
+        token = jwt.sign(user, process.env.SECRET, {
           expiresIn: 86400 // expires in 24 hours
         });
         message = 'Login Successful';
-        break;
+        return res.status(200).json({
+          message,
+          token
+        });
       }
-    }
+    });
+
     // If token is present pass the token to client else send respective message
 
     if (token) {
