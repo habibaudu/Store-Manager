ff9dddeb8e1ba24bff9b6e4227fc7f08695c172c
habib <habib.bincom@gmail.com>
2018-10-28 16:30:22 +0100

    [Chore 161525628]adds db.js to establish a connection

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+   connectionString: process.env.DATABASE_URL
+});
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+require('make-runnable');
+
+
