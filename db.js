cb65ff1bff010bc33377252a96b9c7feb63c26c2
habib <habib.bincom@gmail.com>
2018-10-28 16:59:02 +0100

    [Feature #161531803 ]updates db.js , creates users table

--- db.js
+++ db.js
@@ -11,10 +11,42 @@ pool.on('connect', () => {
 });
 
 
+/**
+ * Create User Table
+ */
+
+const createUserTable = () => {
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(128) NOT NULL,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        Role VARCHAR(11) NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        created_date TIMESTAMP,
+        modified_date TIMESTAMP
+      )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
 pool.on('remove', () => {
   console.log('client removed');
   process.exit(0);
 });
+
+module.exports = {
+  createUserTable
+}
 
 require('make-runnable');
 
