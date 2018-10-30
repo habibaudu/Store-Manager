d814317e3b3354227fadd1e10606c15682d0df63
habib <habib.bincom@gmail.com>
2018-10-30 08:44:12 +0100

    [feature #161578177] updates products controllers to include getAll function

--- server/routes/index.js
+++ server/routes/index.js
@@ -24,14 +24,4 @@ router.delete('/products/:productId',auth.verifyToken,Product.delete);
 
 
 
-// router.get('/products', authroutes, products.getAll);
-// router.put('/products/:productId',authroutes, products.update);
-// router.delete('/products/:productId', authroutes, products.delete);
-// router.get('/products/:productId',authroutes, products.getOne);
-// router.post('/login', validation.login, users.login);
-// router.get('/sales',authroutes, sales.allSales);
-// router.get('/sales/:saleId', authroutes, sales.getAsalesRecord);
-// router.post('/products', authroutes, validation.createproduct, products.create);
-// router.post('/sales',authroutes, sales.createSalesOrder);
-
 export default router;
