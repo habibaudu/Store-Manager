d3e9175628d7f9b82e818912b826f3ba26b3ecbc
habib <habib.bincom@gmail.com>
2018-11-01 01:57:30 +0100

    [Chore #161608110] update travis.yml

--- server/models/migration.js
+++ server/models/migration.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
+console.log(process.env.DATABASE_URL);
 const pool = new Pool({
    connectionString: process.env.DATABASE_URL
 });
