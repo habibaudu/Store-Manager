a66485f87f6f08b327a3a67e7700e82021b2dbb1
habib <habib.bincom@gmail.com>
2018-11-02 05:19:28 +0100

    [bug  #161670412] validate

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -9,60 +9,99 @@
    * @returns {object} users array
    */
 
-class Check {
+  class Check {
   
-  static login(req, res, next) {
-    req.checkBody('password', 'password cannot be null').notEmpty();
-    req.checkBody('username', ' username Must be a String').isAlpha();
-    req.checkBody('username', ' username cannot be null').notEmpty();
-    
-    const errormsg = req.validationErrors();
-    if (errormsg) {
-      res.status(400).send({ message: 'login error', errormsg });
-      return;
+    static login(req, res, next) {
+      req.checkBody('password', 'password cannot be null').notEmpty();
+      req.checkBody('username', ' username Must be a String').isAlpha();
+      req.checkBody('username', ' username cannot be null').notEmpty();
+      
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'login error', errormsg });
+        return;
+      }
+      next();
     }
-    next();
-  }
-
-  static createproduct(req, res, next) {
-    req.checkBody('productName', 'product Name cannot be null').notEmpty();
-
-    req.checkBody('priceEach', 'priceEach cannot be null').notEmpty();
-    req.checkBody('priceEach', 'priceEach Must be a number').isInt();
-    req.checkBody('mininumAllowedinStock', 'mininumAllowedinStock cannot be null').notEmpty();
   
-  
-    req.checkBody('inStock', 'inStock cannot be null').notEmpty();
-  
-
-    const errormsg = req.validationErrors();
-    if (errormsg) {
-      res.status(400).send({ message: 'create product error', errormsg });
-      return;
-    }
-    next();
-  }
-
-
-  static salesRecord(req, res, next) {
-    req.checkBody('product', 'product Name cannot be null').notEmpty();
+    static updateproduct(req, res, next) {
+      req.checkBody('productname', 'product Name cannot be null').notEmpty();
+      req.checkBody('price', 'price  cannot be null').notEmpty();
+      req.checkBody('price', 'price Must be a number').isInt();
+      req.checkBody('minimum', 'minimum can not be empty').notEmpty();
+      req.checkBody('minimum', 'minimum must be a number ').isInt();
+      req.checkBody('quantity', 'quantity can not be empty').notEmpty();
+      req.checkBody('quantitiy', 'quantity must be a number ').isInt();
+      req.checkBody('decription', 'description can not be empty').notEmpty();
+      req.checkBody('description', 'description must be alpha ').isAlpha();
+      req.checkBody('images', 'images can not be empty').notEmpty();
+     
     
-    req.checkBody('price', 'price cannot be null').notEmpty();
-    req.checkBody('price', 'price Must be a number').isInt();
-    req.checkBody('quantity', 'quantity cannot be null').notEmpty();
-    
-    req.checkBody('customerName', 'customerName cannot be null').notEmpty();
-   
-
-    const errormsg = req.validationErrors();
-    if (errormsg) {
-      res.status(400).send({ message: 'create product error', errormsg });
-      return;
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'create product error', errormsg });
+        return;
+      }
+      next();
     }
-    next();
+  
+  
+    static salesRecord(req, res, next) {
+      req.checkBody('product', 'product Name cannot be null').notEmpty();
+      
+      req.checkBody('price', 'price cannot be null').notEmpty();
+      req.checkBody('price', 'price Must be a number').isInt();
+      req.checkBody('quantity', 'quantity cannot be null').notEmpty();
+      
+      req.checkBody('customerName', 'customerName cannot be null').notEmpty();
+     
+  
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'create product error', errormsg });
+        return;
+      }
+      next();
+    }
+  
+    static signup(req, res, next) {
+      req.checkBody('username', 'username  cannot be null').notEmpty()
+      req.checkBody('password', 'password cannot be null').notEmpty();
+      req.checkBody('username', 'username Must be a String').isAlpha();
+      req.checkBody('password', 'password must contain either letters or number').isAlphanumeric();
+      req.checkBody('email', 'email cannot be null').notEmpty();
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'signup error', errormsg });
+        return;
+      }
+      next();
+    }
+  
+    static signin(req, res, next) {
+      req.checkBody('password', 'password cannot be null').notEmpty();
+      req.checkBody('password', 'password must contain either letters or number').isAlphanumeric();
+      req.checkBody('email', 'email cannot be null').notEmpty();
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'login error', errormsg });
+        return;
+      }
+      next();
+    }
+  
+    static giverights(req, res, next) {
+      req.checkBody('Role', 'ROLE  cannot be null').notEmpty();
+      req.checkBody('Role','Role Must be a String').isAlpha();
+      const errormsg = req.validationErrors();
+      if (errormsg) {
+        res.status(400).send({ message: 'events error', errormsg });
+        return;
+      }
+      next();
+    }
+  
   }
-
-}
-
-
-export default Check;
+  
+  
+  export default Check;
