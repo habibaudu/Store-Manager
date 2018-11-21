a7d7c21d0ae62b65eeee99eed0ac01a05d9274a9
habib <habib.bincom@gmail.com>
2018-11-21 05:39:33 +0100

    [feature #162105858] adds allUsers.html and allUsers.js. Consumes all Users endpoint

---
+++ client/allUsers.js
@@ -0,0 +1,67 @@
+window.addEventListener("load",getUsers);
+
+function getUsers(){
+            
+    token = localStorage.getItem('token')
+    fetch('http://localhost:4000/api/v1/users', {
+        method: 'GET',
+        mode:'cors',
+        headers: {'Content-Type': 'application/json',
+                   'x-access-token':token,
+                   'pragma':'no-cache', 
+                   'cache-control': 'no-cache'               
+                           
+                  }
+       
+    }).then((Response) =>{
+        status = Response.status;
+        return Response.json();
+    })
+    .then((data) => {
+        switch(status){
+            case '200':
+                  
+                   data2 = data.rows
+                    let users =` <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Date Created</th>
+                    <th>Date Modified</th>
+             
+                  </tr>`;
+                    data2.forEach((user) => {
+                        const {id,username, role, email,modified_date, created_date} = user;
+                   
+                        users +=
+                            `<tr>
+                            <td>${id}</td>
+                            <td>${username}</td>
+                            <td>${email}</td>
+                            <td>${role}</td>
+                           
+                            <td>${created_date}</td>
+                            <td>${modified_date}</td>
+                          </tr>`;
+                                document.getElementById('allusers').innerHTML = users;
+                        })
+                    
+                        break;
+            case '401': 
+                            
+                        alert(`${data.message}`);
+                        window.location = 'login.html'
+                        break;
+                    
+            default:
+                
+                        alert(`${data.message}`);
+                        window.location = 'index.html';
+                        break;
+                        
+
+        }
+})  
+    .catch((err)=>console.log(err));
+}
